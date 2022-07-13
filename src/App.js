ae264af646e1b8b486e4a4448d1b79c50f47dcf4
sunbo <wjdtnsqh@naver.com>
2022-07-13 16:36:46 +0900

    라우터랑 로그인페이지

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import "./App.scss";
 import Footer from "./components/Include/Footer";
 import Header from "./components/Include/Header";
 import Index from './components/WheelHandler';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from "./components/Login/LoginPage";
+import ResPage from "./components/Reservation/ResPage";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Index/>
+      <Routes>
+        <Route path="/" element={<Index/>}/>
+        <Route path="/reservation/" element={<ResPage/>}/>
+        <Route path="/login/" element={<LoginPage/>}/>
+        <Route path="/login/" element={<AboutUs/>}/>
+      </Routes>
       <Footer/>
     </div>
   );
