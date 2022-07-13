2b90163eb112019f1814fcb0a4fd2e3755692f96
LeegukHyoun <dbkim512@naver.com>
2022-07-13 12:21:49 +0900

    dd

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Activity = () => {
+    
     return (
             <div id="activity" className='height'>
-                <div className='actText'></div>
-                <div>
-                    <img src='' alt='' />
+                <div className='actText'></div> 
+                <div> 
+                    <div id='ActBg'></div>
+                    <img src='images/ttt-12.png' alt='' />
                 </div>
-                {/* 구현이 된다면 흘러가는 슬라이더로 하기 */}
-            ㅇㅇㅇㅇ간지
             </div>
 
     );
--- src/components/ReservationBar.js
+++ src/components/ReservationBar.js
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BsFillCalendarFill} from "react-icons/bs"
+import {AiFillPlusSquare} from "react-icons/ai"
+import {AiFillMinusSquare} from "react-icons/ai"
 
-const ReservationBar = ({selectDate,selectDate2}) => {
+
+const ReservationBar = ({selectDate,selectDate2, CalOn}) => {
+
+    const [ Select, SetSelect ] = useState("Strato");
+    const [ adultNum, SetAdultNum ] = useState(0);
+    const [ KidsNum, SetKidsNum ] = useState(0);
+
+    const onChange = (e) => {
+        SetSelect(e.target.value);
+    }
+
+    const onAdultPlus = () => {
+        SetAdultNum(adultNum + 1);
+    }
+    const onAdultMinus = () => {
+        if(adultNum > 0){
+            SetAdultNum(adultNum - 1);
+        }
+    }
+    const onKidsPlus = () => {
+        SetKidsNum(KidsNum + 1);
+    }
+    const onKidsMinus = () => {
+        if(KidsNum > 0){
+            SetKidsNum(KidsNum - 1);
+        }
+    }
     return (
         <>
             <ul>
-                <li>ROOM</li>
+                <li>
+                <select name="rooms" id="rooms" value={Select} onChange={onChange}>
+                    <option value="Strato" className='option'>Strato Room</option>
+                    <option value="Sweet" className='option'>Sweet Room</option>
+                    <option value="Busyness" className='option'>Busyness Room</option>
+                </select>
+                </li>
                 <li>
                     <span>CHECK IN / OUT</span>
-                    <span><span className='num'>{selectDate}</span> ~ <span className='num'>{selectDate2}<BsFillCalendarFill className='calIcon' /></span></span>
+                    <span><span className='num'>{selectDate}</span> - <span className='num'>{selectDate2}<BsFillCalendarFill className='calIcon'  onClick={CalOn}/></span></span>
                 </li>
                 <li>
-                    <span>Room <span className='num'>1</span></span>
-                    <span>ADULT <span className='num'>2</span></span>
-                    <span>CHILDREN <span className='num'>0</span></span>
+                    <span id='RoomNum'><span className='num'>{Select}</span></span>
+                    <span>ADULT　 <span className='num'>{adultNum}</span><AiFillPlusSquare className='arrow' onClick={onAdultPlus}/><AiFillMinusSquare className='arrow' onClick={onAdultMinus}/></span>
+                    <span>KIDS　　 <span className='num'>{KidsNum}</span><AiFillPlusSquare className='arrow' onClick={onKidsPlus}/><AiFillMinusSquare className='arrow' onClick={onKidsMinus}/></span>
                 </li>
             </ul>
+            <div>
+                <button id='searchBtn'>Reservation</button>
+            </div>
         </>
     );
 };
