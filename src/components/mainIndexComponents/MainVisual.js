c5578d8c28773605a9809429d3a5256385030600
hyunjinP <wldwlddl0531@gmail.com>
2022-07-14 17:47:58 +0900

    roomDetail 70% 완료

--- src/components/mainIndexComponents/MainVisual.js
+++ src/components/mainIndexComponents/MainVisual.js
@@ -16,22 +16,22 @@ const MainVisual = () => {
             <Carousel effect="fade" autoplay>
                 <div>
                 <h3 style={contentStyle}>
-                    <img src="images/KV1.png"/>
+                    <img src="images/KV1.png" alt='메인사진1'/>
                 </h3>
                 </div>
                 <div>
                 <h3 style={contentStyle}>
-                    <img src="images/KV2.png"/>
+                    <img src="images/KV2.png" alt='메인사진2'/>
                 </h3>
                 </div>
                 <div>
                 <h3 style={contentStyle}>
-                    <img src="images/KV3.png"/>
+                    <img src="images/KV3.png" alt='메인사진3'/>
                 </h3>
                 </div>
                 <div>
                 <h3 style={contentStyle}>
-                    <img src="images/KV_summer_1.png"/>
+                    <img src="images/KV_summer_1.png" alt='메인사진4'/>
                 </h3>
                 </div>
             </Carousel>
