8b951035f38ce8149d4afb5fa76b17810e49ac53
sunbo <wjdtnsqh@naver.com>
2022-07-15 13:58:13 +0900

    aboutus

--- src/components/detail/AboutUs.js
+++ src/components/detail/AboutUs.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div className='bg'>
-            
+        <div id="brandS">
+            <div id="brandS-top">
+                <img src='../images/aboutus.jpg' alt="aboUstusImg"/>
+                <h2>Strato Story</h2>
+            </div>
+            <div id="aboutCon">
+                <div id="brandS-t">
+                    <h1>Brand Story</h1>
+                    <h2>
+                        자연 속 편안한 휴식을 즐기는
+                        사계절 올인클루시브 스트라토
+                    </h2>
+                    <p> 
+                        스트라토가 이어나가야 할 자산은
+                        고객의 마음과 시간을 사고, 고객에게 행복한 경험을 선물하는 것입니다.
+                        다양한 사계절의 모습을 만날 수 있는 평창과 제주에서
+                        소중한 이들과 마음 편히, 여유로운 휴식을 즐겨보시기 바랍니다.
+                    </p>
+                </div>
+                <div id="brandS-m">
+                    <div>
+                        <h2>CHECK IN TO NATURE</h2>
+                        <p>
+                            푸른 숲속 여유롭게 편안한 휴식을 취하며 머무르는 곳, 휘닉스 평창
+                            푸른 제주 바다를 배경으로 사계절 즐거움이 피어나는 곳, 휘닉스 제주 섭지코지
+                            자연으로 떠나는 여행의 로망이 실현되는 곳,
+                            여기는 “Strato” 입니다.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
