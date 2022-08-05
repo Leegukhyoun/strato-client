a82ce3ae290ff46468959548e2334cfc595490db
hyunjinP <wldwlddl0531@gmail.com>
2022-08-05 17:52:05 +0900

    2022-08-05 금요일

--- src/components/detail/Signup.js
+++ src/components/detail/Signup.js
@@ -1,41 +1,41 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import PopupPostCode from './AddressSearch/PopupPostCode';
 import PopupDom from './AddressSearch/PopupDom';
 
-const Signup = ({onChange, onHome, onSubmit, createUser}) => {
+const Signup = ({ onChange, onHome, onSubmit, createUser }) => {
     console.log(onChange, createUser)
-    
+
     //우편번호 관리
     const onAddData = (data) => {
         console.log(data);
         const postAdd = data.address;
         onChange({
-            target : {
-                name : 'addr1',
-                value : postAdd
+            target: {
+                name: 'addr1',
+                value: postAdd
             }
         })
     }
 
-    
-    const [ isPopupOpen, setIsPopupOpen ] = useState(false);
-    
-    
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+
     const openPostCode = (e) => {
         e.preventDefault();
         setIsPopupOpen(true);
     }
-    
+
     const closePostCode = (e) => {
         e.preventDefault();
         setIsPopupOpen(false);
     }
     //우편번호 관리 끝
-    
+
 
     const onSubmitch = (e) => {
         e.preventDefault();
-        if(createUser.name !== "" && createUser.phone !== "" && createUser.birth !== "" && createUser.gender !== "" && createUser.addr1 !== ""){
+        if (createUser.name !== "" && createUser.phone !== "" && createUser.birth !== "" && createUser.gender !== "" && createUser.addr1 !== "") {
             onSubmit();
             onHome();
         }
@@ -52,7 +52,7 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
             <div className="res-confirm contentInner">
                 <h2>회원 정보 입력</h2>
                 <div className="mWrap loginBox ">
-                    <form  onSubmit={onSubmitch}>
+                    <form onSubmit={onSubmitch}>
                         <ul>
                             <li>
                                 <div className="textBox">
@@ -63,7 +63,7 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
                                 <div className="textBox">
                                     <p>＊ 이름</p>
                                     <p>
-                                        <input name="name" type="text" placeholder="아이디를 입력해주세요." value={createUser.name} onChange={onChange}/>
+                                        <input name="name" type="text" placeholder="아이디를 입력해주세요." value={createUser.name} onChange={onChange} />
                                     </p>
                                 </div>
                             </li>
@@ -71,7 +71,7 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
                                 <div className="textBox" >
                                     <p>＊ 전화번호</p>
                                     <p>
-                                        <input name="phone" type="text" placeholder="연락처를 입력해주세요." value={createUser.phone} onChange={onChange}/>
+                                        <input name="phone" type="text" placeholder="연락처를 입력해주세요." value={createUser.phone} onChange={onChange} />
                                     </p>
                                 </div>
                             </li>
@@ -79,7 +79,7 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
                                 <div className="textBox">
                                     <p>＊ 생년월일</p>
                                     <p>
-                                        <input name="birth" type="text" placeholder="생년월일을 입력해주세요." value={createUser.birth} onChange={onChange}/>
+                                        <input name="birth" type="text" placeholder="생년월일을 입력해주세요." value={createUser.birth} onChange={onChange} />
                                     </p>
                                 </div>
                             </li>
@@ -87,8 +87,8 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
                                 <div className="textBox">
                                     <p>＊ 생년월일</p>
                                     <p>
-                                        <span>남성</span><input name="gender" type="radio" value="남성" className="radioInput" onChange={onChange}/>
-                                        <span>여성</span><input name="gender" type="radio" value="여성" className="radioInput" onChange={onChange}/>
+                                        <span>남성</span><input name="gender" type="radio" value="남성" className="radioInput" onChange={onChange} />
+                                        <span>여성</span><input name="gender" type="radio" value="여성" className="radioInput" onChange={onChange} />
                                     </p>
                                 </div>
                             </li>
@@ -96,16 +96,16 @@ const Signup = ({onChange, onHome, onSubmit, createUser}) => {
                                 <div className="textBox">
                                     <p>＊ 주소 <button className="inputButton" onClick={openPostCode}>주소 찾기</button></p>
                                     <p>
-                                        <input type="text" placeholder="주소를 입력하거나 찾으세요." name="addr1" value={createUser.addr1} onChange={onChange}/>
-                                        <input type="text" placeholder="상세 주소를 입력하세요." name="addr2" value={createUser.addr2} onChange={onChange}/>
+                                        <input type="text" placeholder="주소를 입력하거나 찾으세요." name="addr1" value={createUser.addr1} onChange={onChange} />
+                                        <input type="text" placeholder="상세 주소를 입력하세요." name="addr2" value={createUser.addr2} onChange={onChange} />
                                     </p>
                                     <div id="popupDom">
-                                    {isPopupOpen && (
-                                        <PopupDom>
-                                            <PopupPostCode onClose={closePostCode} onAddData={onAddData}/>
-                                        </PopupDom>
-                                    )}
-                                </div>
+                                        {isPopupOpen && (
+                                            <PopupDom>
+                                                <PopupPostCode onClose={closePostCode} onAddData={onAddData} />
+                                            </PopupDom>
+                                        )}
+                                    </div>
                                 </div>
                             </li>
                             <li>
